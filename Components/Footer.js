6a8a10b22f0c29c15765368c0f755733aca7156e
kandurisv <30374472+kandurisv@users.noreply.github.com>
2021-06-30 22:15:57 +0530

    commit 1

---
+++ Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/componentStyles.module.css'
+
+function Footer() {
+    return (
+        <div className = {styles.footer}>
+            <h3>This is footer component</h3>
+        </div>
+    )
+}
+
+export default Footer
---
+++ Components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/componentStyles.module.css'
+
+function Hero() {
+    return (
+        <div className = {styles.hero}>
+            <h1>this is hero component</h1>
+        </div>
+    )
+}
+
+export default Hero
