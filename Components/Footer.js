ee408748aabc25e4cd9f2c31ee7854e18fa8ea08
kandurisv <30374472+kandurisv@users.noreply.github.com>
2021-07-27 18:45:44 +0530

    SEO Optimization - 1

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,9 +1,26 @@
-function Footer() {
-    return (
-        <div>
-            <h1>This is footer</h1>
-        </div>
-    )
+import Image from 'next/image'
+import Link from 'next/link'
+import HeaderItem from './HeaderItem'
+import React from 'react'
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
+
+
+const Footer = () => {
+
+  
+  return (
+      <div className = "flex flex-row items-center  justify-start bg-white ">
+        <Link href={"https://www.facebook.com/getcandidapp/"} passHref> 
+            <div className = "cursor-pointer mx-3"><FaFacebook /></div> 
+        </Link>
+        <Link href={"https://www.instagram.com/getcandidapp/"} passHref>
+            <div className = "cursor-pointer mx-3"> <FaInstagram /> </div>
+        </Link>
+        <Link href={"https://www.twitter.com/getcandidapp/"} passHref> 
+            <div className = "cursor-pointer mx-3"> <FaTwitter /> </div>
+        </Link>
+      </div>
+  )
 }
 
 export default Footer
